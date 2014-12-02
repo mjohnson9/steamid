49e5c85c6cad4c28acf5fec314ffa1dae41b1c98
Michael Johnson <michael@nightexcessive.us>
2014-12-02 17:47:23 -0500

    Add basic tests

---
+++ steamid_test.go
@@ -0,0 +1,35 @@
+package steamid_test
+
+import (
+	"testing"
+
+	"github.com/nightexcessive/steamid"
+)
+
+var steamIDTests = []struct {
+	Universe    uint8
+	Instance    uint32
+	Type        uint8
+	ID          uint32
+	ExpectedID2 string
+	ExpectedID3 string
+}{
+	{0, 1, 1, 2, "STEAM_0:0:1", "[U:0:2]"},
+	{1, 1, 1, 35812865, "STEAM_0:1:17906432", "[U:1:35812865]"},
+}
+
+func TestSteamID(t *testing.T) {
+	for _, details := range steamIDTests {
+		steamID := steamid.FromValues(details.Universe, details.Instance, details.Type, details.ID)
+
+		got2 := steamID.SteamID2()
+		if got2 != details.ExpectedID2 {
+			t.Errorf("SteamID2([Universe %d Instance %d Type %d ID %d]): got %q, expected %q", details.Universe, details.Instance, details.Type, details.ID, got2, details.ExpectedID2)
+		}
+
+		got3 := steamID.SteamID3()
+		if got3 != details.ExpectedID3 {
+			t.Errorf("SteamID3([Universe %d Instance %d Type %d ID %d]): got %q, expected %q", details.Universe, details.Instance, details.Type, details.ID, got3, details.ExpectedID3)
+		}
+	}
+}
